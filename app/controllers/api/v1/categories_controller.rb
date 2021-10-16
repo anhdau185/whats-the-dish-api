a53b67b981d84b8d8232c330369c0b47ffe8f058
Anh Dau <anh.dau@employmenthero.com>
2021-10-16 17:21:03 +0700

    Remove rails view

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -3,8 +3,6 @@
 module Api
   module V1
     class CategoriesController < ApplicationController
-      before_action :prepare_category
-
       def index
         categories = Category.all
         render json: categories, include: ['dishes']
