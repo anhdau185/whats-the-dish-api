fc25b7cad7f537ebc149c604c64623a8e2d06fdd
Anh Dau <anh.dau@employmenthero.com>
2021-10-17 15:08:29 +0700

    Finish basic CRUD actions for categories api

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -12,6 +12,42 @@ module Api
         category = Category.find(params[:id])
         render json: category, include: ['dishes']
       end
+
+      def create
+        category_details = params
+          .require(:attributes)
+          .permit(:name, :title, :description, images: [])
+        category = Category.new(category_details)
+
+        if category.save
+          render status: 201, json: category
+        else
+          render status: 400
+        end
+      end
+
+      def update
+        category = Category.find(params[:id])
+        category_details = params
+          .require(:attributes)
+          .permit(:name, :title, :description, images: [])
+
+        if category.update(category_details)
+          render json: category, include: ['dishes']
+        else
+          render status: 400
+        end
+      end
+
+      def destroy
+        category = Category.find(params[:id])
+
+        if category.destroy
+          render status: 204
+        else
+          render status: 400
+        end
+      end
     end
   end
 end
