31e0c3d72f2c1a2912386da705067332bc8961ce
Anh Dau <redbeanboy98@gmail.com>
2021-10-22 10:14:35 +0700

    Add name validation to category and dish

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,5 @@ class Category < ApplicationRecord
   has_many :category_assignments, dependent: :destroy
   has_many :dishes, through: :category_assignments
 
-  validates :name, presence: true
+  validates :name, presence: true, length: { maximum: 50 }
 end
--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,4 +1,6 @@
 class Dish < ApplicationRecord
   has_many :category_assignments, dependent: :destroy
   has_many :categories, through: :category_assignments
+
+  validates :name, presence: true, length: { maximum: 70 }
 end
